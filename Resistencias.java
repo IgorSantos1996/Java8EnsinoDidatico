c0ee4a52f54a579049396321cf8f780737085027
IgorSantos1996 <igsantos1996@gmail.com>
2018-11-05 14:05:35 -0300

    Alterações nas classes

--- Resistencias.java
+++ Resistencias.java
@@ -9,35 +9,36 @@ public class Resistencias {
         resistencia02 = Integer.parseInt(JOptionPane.showInputDialog("Entre com a segunda resistencia: "));
         resistencia03 = Integer.parseInt(JOptionPane.showInputDialog("Entre com a terceira resistencia: "));
         resistencia04 = Integer.parseInt(JOptionPane.showInputDialog("Entre com a quarta resistencia: "));
-        maior = 0;
+
+        maior = resistencia01;
         menor = 0;
-        if (maior < resistencia01){
+        if (maior < resistencia01) {
             maior = resistencia01;
-        }else{
-            menor = maior;
+        } else {
+            menor = resistencia01;
         }
         if (maior < resistencia02) {
             maior = resistencia02;
 
         } else {
-            menor = maior;
+            menor = resistencia02;
         }
         if (maior < resistencia03) {
             maior = resistencia03;
 
         } else {
-            menor = maior;
+            menor = resistencia03;
         }
         if (maior < resistencia04) {
             maior = resistencia04;
 
         } else {
-            menor = maior;
+            menor = resistencia04;
         }
-        soma = resistencia01 + resistencia02 + resistencia03 + resistencia04;
+        soma = resistencia01 + resistencia02 + resistencia03 + resistencia04 / 4;
         JOptionPane.showMessageDialog(null,
-                "Resistencia fornecidas: " + resistencia01 + ", " + resistencia02 + ", " + resistencia03 + ", "
-                        + resistencia04 + "\n" + "A maior resistência é: " + maior + "\n" + "A menor resistencia é: "
-                        + menor);
+                "A média das resistências é " + soma + "\nResistencia fornecidas: " + resistencia01 + ", " + resistencia02
+                        + ", " + resistencia03 + ", " + resistencia04 + "\n" + "A maior resistência é: " + maior + "\n"
+                        + "A menor resistencia é: " + menor);
     }
 }
